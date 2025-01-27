936858617ba19c6f40a6f2a2161048e075afab5b
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-01-27 23:20:47 +0000

    connection request

--- src/app.js
+++ src/app.js
@@ -9,8 +9,9 @@ app.use(cookieParser());
 const router = require("./routes/auth")
 const profileRouter = require("./routes/profile")
 const requestRouter = require("./routes/request")
+const userRouter = require("./routes/user");
 
-app.use("/", router, profileRouter, requestRouter);
+app.use("/", router, profileRouter, requestRouter, userRouter);
 connectDB()
   .then(() => {
     console.log("Database connection established...");
