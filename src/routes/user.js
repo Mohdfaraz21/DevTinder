549a094385ae6243319ec9b5b11155130f5b5ced
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-01-28 07:13:19 +0000

    request received

--- src/routes/user.js
+++ src/routes/user.js
@@ -34,7 +34,7 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       .populate("fromUserId", USER_SAFE_DATA)
       .populate("toUserId", USER_SAFE_DATA);
 
-    const data = connectionRequests.map((row) => {
+    const data = connections.map((row) => {
       if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
         return row.toUserId;
       }
